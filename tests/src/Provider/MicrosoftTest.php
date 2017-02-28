524dadb44f758683ab8fe294e9534ac5a892c16f
Steven Maguire <stevenmaguire@gmail.com>
2017-02-27 20:54:00 -0600

    Update scope test

--- tests/src/Provider/MicrosoftTest.php
+++ tests/src/Provider/MicrosoftTest.php
@@ -1,9 +1,12 @@
 <?php namespace Stevenmaguire\OAuth2\Client\Test\Provider;
 
+use League\OAuth2\Client\Tool\QueryBuilderTrait;
 use Mockery as m;
 
 class MicrosoftTest extends \PHPUnit_Framework_TestCase
 {
+    use QueryBuilderTrait;
+
     protected $provider;
 
     protected function setUp()
@@ -46,11 +49,12 @@ class MicrosoftTest extends \PHPUnit_Framework_TestCase
 
     public function testScopes()
     {
-        $options = ['scope' => [uniqid(),uniqid()]];
-
+        $scopeSeparator = ',';
+        $options = ['scope' => [uniqid(), uniqid()]];
+        $query = ['scope' => implode($scopeSeparator, $options['scope'])];
         $url = $this->provider->getAuthorizationUrl($options);
-
-        $this->assertContains(urlencode(implode(',', $options['scope'])), $url);
+        $encodedScope = $this->buildQueryString($query);
+        $this->assertContains($encodedScope, $url);
     }
 
     public function testGetBaseAccessTokenUrl()
